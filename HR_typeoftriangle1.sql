89afb83307ea1aa8ef4c32eb1e8176d39c8d4345
deepsshinde <120267784+deepsshinde@users.noreply.github.com>
2024-02-19 14:05:18 +0530

    Create HR_typeoftriangle1.sql

---
+++ HR_typeoftriangle1.sql
@@ -0,0 +1,13 @@
+/*
+Enter your query here.
+*/
+
+SELECT
+    CASE
+        WHEN A + B <= C OR B + C <= A OR A + C <= B THEN 'Not A Triangle'
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END AS TRIANGLE_TYPE
+FROM
+    TRIANGLES;
